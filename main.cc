f6ee20adff8558bfa3c2c7465babd0e9f7f7624c
Matt Davis <mattdavis9@gmail.com>
2019-01-05 18:17:01 -0800

    Update

--- main.cc
+++ main.cc
@@ -10,13 +10,31 @@
 #include <Observer.h>
 #include <SGP4.h>
 
+#define DEFAULT_DB_PATH "./.satnow.sql3"
+
+// Command line options.
+static const struct option opts[] = {
+  {"alt", required_argument, nullptr,    'a'},
+  {"lat", required_argument, nullptr,    'x'},
+  {"lon", required_argument, nullptr,    'y'},
+  {"update", required_argument, nullptr, 'u'},
+  {"db", required_argument, nullptr, 'd'}
+};
+
 [[ noreturn ]] static void usage(const char *execname) {
   std::cout << "Usage: " << execname
-            << " [file.tle ...] [--lat=val --lon=val --alt=val]"
+            << " [file.tle ...] [--lat=val --lon=val --alt=val --update=file --db=file]"
             << std::endl
             << "  --lat=<latitude in degrees>" << std::endl
             << "  --lon=<longitude in degrees>" << std::endl
-            << "  --alt=<altitude in meters>" << std::endl;
+            << "  --alt=<altitude in meters>" << std::endl
+            << "  --db=<path to database> (default: " << DEFAULT_DB_PATH << ')'
+            << std::endl
+            << "  --update=<sources>  " << std::endl
+            << "    Where 'sources' is a text file containing a " << std::endl
+            << "    list of URLs that point to a TLE .txt file. " << std::endl
+            << "    Each source must be on its own line in the file."
+            << std::endl;
   exit(EXIT_SUCCESS);
 }
 
@@ -52,22 +70,60 @@ static std::vector<Tle> readTLEs(const char *fname) {
   return tles;
 }
 
-static const struct option opts[] = {
-  {"alt", required_argument, nullptr, 'a'},
-  {"lat", required_argument, nullptr, 'x'},
-  {"lon", required_argument, nullptr, 'y'}
-};
+static bool isURL(const std::string &str) { return false; }
+static bool isFile(const std::string &str) { return false; }
+
+// Update the database of TLEs.
+static void update(const char *sourceFile, const char *dbPath) {
+  // Open database.
+  // TODO
+  
+  // Parse the source file (one entry per line).
+  std::string line;
+  std::ifstream fh(sourceFile);
+  size_t lineNumber = 0;
+  while (std::getline(fh, line)) {
+    ++lineNumber;
+    // Trim white space.
+    size_t st, en;
+    if ((st = line.find_first_not_of(" \t\r\n")) == std::string::npos ||
+        (en = line.find_last_not_of(" \t\r\n")) == std::string::npos)
+      continue;
+    // If a comment line, ignore.
+    if (line[st] == '#') continue;
+    
+    // Reject empty lines.
+    if (en  - st == 0)
+      continue;
+
+    // Get the trimmed string.
+    auto str = line.substr(st, en);
+
+    // If URL download the TLEs.
+    if (isURL(str)) {
+    }
+    else if (isFile(str)) {
+    }
+    else {
+      std::cerr << "[-] Unknown entry in " << sourceFile << " Line " << lineNumber << std::endl;
+      continue;
+    }
+  }
+}
 
 int main(int argc, char **argv) {
   // Handle arguments.
   double alt = 0.0, lat = 0.0, lon = 0.0;
+  const char *sourceFile = nullptr, *dbFile = DEFAULT_DB_PATH;
   int opt;
-  while ((opt = getopt_long(argc, argv, "ha:x:y:", opts, nullptr)) > 0) {
+  while ((opt = getopt_long(argc, argv, "ha:x:y:u:d:", opts, nullptr)) > 0) {
     switch (opt) {
       case 'h': usage(argv[0]); break;
       case 'a': alt = std::stod(optarg); break;
       case 'x': lat = std::stod(optarg); break;
       case 'y': lon = std::stod(optarg); break;
+      case 'u': sourceFile = optarg; break;
+      case 'd': dbFile = optarg; break;
       default: {
          std::cerr <<  "[-] Unknown command line option." << std::endl;
          exit(EXIT_FAILURE);
@@ -75,6 +131,9 @@ int main(int argc, char **argv) {
       }
     }
   }
+
+  // If a source file is specified, then update the existing database.
+  update(sourceFile, dbFile);
 
   // Build the observer (user's position)  
   auto me = Observer(37.584, -122.366, 0.0);
